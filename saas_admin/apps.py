0d867f1ec606e71d1ecd274797b074ad3eac312c
profAlbertoAyres <alberto.benicio@ifro.edu.br>
2026-04-27 21:46:45 -0400

    Começando o SaaS

---
+++ saas_admin/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class SaasAdminConfig(AppConfig):
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "saas_admin"
+    verbose_name = "SaaS Admin"
---
+++ saas_admin/views/base.py
@@ -0,0 +1,39 @@
+import logging
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import Http404
+
+logger = logging.getLogger(__name__)
+
+
+class SaaSAdminRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+
+    raise_exception = True
+
+    def test_func(self) -> bool:
+        user = self.request.user
+
+
+        if not user.is_authenticated:
+            return False
+
+        if not user.is_active:
+            return False
+
+
+        return getattr(user, "is_saas_staff", False)
+
+    def handle_no_permission(self):
+
+        user = self.request.user
+        user_repr = (
+            f"user_id={user.pk}" if user.is_authenticated else "anonymous"
+        )
+
+        logger.warning(
+            "Acesso negado ao SaaS Admin: %s tentou acessar %s",
+            user_repr,
+            self.request.path,
+        )
+
+        raise Http404("Página não encontrada.")
